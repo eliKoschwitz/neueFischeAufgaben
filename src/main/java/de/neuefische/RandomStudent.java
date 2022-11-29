795e0dc843e074083b65cdf1b679840fae270a4f
elikoschwitz <e.koschwitz@googmail.com>
2022-11-29 12:21:51 +0100

    Started with the Task Random Student

--- src/main/java/de/neuefische/RandomStudent.java
+++ src/main/java/de/neuefische/RandomStudent.java
@@ -1,4 +1,5 @@
 package de.neuefische;
 
 public class RandomStudent {
+
 }
---
+++ src/test/java/de/neuefische/RandomStudentTest.java
@@ -0,0 +1,19 @@
+package de.neuefische;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomStudentTest {
+
+    @Test
+    void construktorCheck(){
+        // GIVEN
+        Student[] arr = {new Student(), new Student()};
+        StudentDB db = new StudentDB(arr);
+        // WHEN
+
+        // THEN
+
+    }
+}
