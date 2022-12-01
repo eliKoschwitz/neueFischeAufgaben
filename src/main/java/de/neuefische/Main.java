fc8cdc2bf0f3027f563f01caaed34bb86cfbe7d1
elikoschwitz <e.koschwitz@googmail.com>
2022-12-01 11:52:08 +0100

    Init: Big Bang

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -1,7 +1,12 @@
 package de.neuefische;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+        LocalDate localDate = LocalDate.now(ZoneId.of("Europe/Berlin"));
+        System.out.println(localDate);
     }
 }
